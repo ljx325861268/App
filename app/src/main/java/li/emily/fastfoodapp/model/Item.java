7d4ced2b070c4ed9dcc174182f590a75e2c80663
Emily Li <z5110645@ad.unsw.edu.au>
2019-10-14 19:03:47 +1100

    Item: added description ItemDatabase: Added static items to database

--- app/src/main/java/li/emily/fastfoodapp/model/Item.java
+++ app/src/main/java/li/emily/fastfoodapp/model/Item.java
@@ -6,12 +6,14 @@ public class Item {
     private int imageID; // unique identifier of the image associated with the item
     private String name;
     private double price;
+    private String description;
 
-    public Item(int id, int imageID, String name, double price) {
+    public Item(int id, int imageID, String name, double price, String description) {
         this.id = id;
         this.imageID = imageID;
         this.name = name;
         this.price = price;
+        this.description = description;
     }
 
     public int getId() {
@@ -44,5 +46,13 @@ public class Item {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
--- app/src/main/java/li/emily/fastfoodapp/model/ItemDatabase.java
+++ app/src/main/java/li/emily/fastfoodapp/model/ItemDatabase.java
@@ -1,5 +1,25 @@
 package li.emily.fastfoodapp.model;
 
+import java.util.HashMap;
+
 public class ItemDatabase {
-    
+    private static final HashMap<Integer, Item> items = new HashMap<Integer,Item>();
+
+    static{
+        items.put(1, new Item(1, 1234, "Hamburger", 2.00, "abc"));
+        items.put(2, new Item(2, 1234, "Cheeseburger", 3.00, "abc"));
+        items.put(3, new Item(3, 1234, "Big Mac", 7.00, "abc"));
+        items.put(4, new Item(4, 1234, "Small Mac", 5.00, "abc"));
+        items.put(5, new Item(5, 1234, "Medium Mac", 6.00, "abc"));
+        items.put(6, new Item(6, 1234, "Small Fries", 3.00, "abc"));
+        items.put(7, new Item(7, 1234, "Medium Fries", 4.00, "abc"));
+        items.put(8, new Item(8, 1234, "Large Fries", 5.00, "abc"));
+        items.put(9, new Item(9, 1234, "Small Ice Cream", 2.00, "abc"));
+        items.put(10, new Item(10, 1234, "Large Ice Cream", 2.00, "abc"));
+        items.put(11, new Item(11, 1234, "Coffee", 2.00, "abc"));
+        items.put(12, new Item(12, 1234, "Small Soft Drink", 2.00, "abc"));
+        items.put(13, new Item(13, 1234, "Medium Soft Drink", 2.00, "abc"));
+        items.put(14, new Item(14, 1234, "Large Soft Drink", 2.00, "abc"));
+        items.put(15, new Item(15, 1234, "Water", 2.00, "abc"));
+    }
 }
